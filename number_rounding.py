120907dccb6d427a351b5019586441093fe1ecd3
danielp67 <daniel.67100@hotmail.fr>
2024-07-06 21:35:12 +0200

    test des env virtuel avec python

--- number_rounding.py
+++ number_rounding.py
@@ -6,3 +6,9 @@ print(numpy.ceil(1.2))
 print(numpy.ceil(1.933))
 
 print(numpy.floor(1.2))
+
+print(numpy.std([1, 17, 32, 4]))
+
+
+print(numpy.linalg.det(numpy.array([[1, 11], [4, 12]])))
+print(numpy.linalg.det(numpy.array([[31, 22], [22, 31]])))
